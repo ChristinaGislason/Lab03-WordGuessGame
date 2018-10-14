0348b97cb07a9860a6ca9641121a305f0925c857
Christina Gislason <christinamkcho@gmail.com>
2018-10-13 20:37:09 -0700

    initial setup for program; errors

--- Lab03-WordGuessGame/Lab03-WordGuessGame/Program.cs
+++ Lab03-WordGuessGame/Lab03-WordGuessGame/Program.cs
@@ -8,9 +8,58 @@ namespace Lab03_WordGuessGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome, let\'s play a Word Guess Game!");
+            WordGuessGame();
+            
+
+
+
             CreateFile();
         }
+
+        static void WordGuessGame() 
+        {
+            bool action = true;
+            while (action) 
+            {              
+                Console.WriteLine("Please select an option:");
+                Console.WriteLine("1. Start a Game");
+                Console.WriteLine("2. Admin");
+                Console.WriteLine("3. Exit Game");
+                int option;
+
+                try
+                {
+                    option = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    option = 5;
+                }
+
+                switch (option)
+                {
+                    case 1:
+                        StartGame();
+                        break;
+                    case 2:
+                        RunAdmin();
+                        break;
+                    case 3:                                             
+                        Environment.Exit(0);
+                        break;                                     
+                    default:
+                        Console.Write("Invalid option. Please choose an option.");                    
+                        break;    
+                } 
+            }
+        }
+
+        ViewAllWords();
+        AddAWord();
+        DeleteAWord();
+        ReturnToMainMenu();
+
 
         static void CreateFile()
         {
